a244a0ce8ce467d64cc0faea4998c67c885ca437
tobias <tobias.helfrich@rewoo.com>
2019-01-22 14:41:32 +0100

    removed unused classes and elastic declarations; renamed providers to meet OIH conventions

--- src/main/java/com/rewoo/elastic/actions/TransformDocumentFromOih.java
+++ src/main/java/com/rewoo/elastic/actions/TransformDocumentFromOih.java
@@ -5,8 +5,8 @@ import io.elastic.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TransformToOih implements Module {
-    private static final Logger logger = LoggerFactory.getLogger(TransformToOih.class);
+public class TransformDocumentFromOih implements Module {
+    private static final Logger logger = LoggerFactory.getLogger(TransformDocumentFromOih.class);
 
     @Override
     public void execute(ExecutionParameters parameters) {
--- src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
+++ src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
@@ -1,16 +1,12 @@
 package com.rewoo.elastic.actions;
 
 import io.elastic.api.ExecutionParameters;
-import io.elastic.api.Message;
 import io.elastic.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.json.JsonObject;
-import javax.json.JsonString;
-
-public class TransformFromOih implements Module {
-    private static final Logger logger = LoggerFactory.getLogger(TransformFromOih.class);
+public class TransformDocumentToOih implements Module {
+    private static final Logger logger = LoggerFactory.getLogger(TransformDocumentToOih.class);
 
     @Override
     public void execute(ExecutionParameters parameters) {
