a72003aace577e29c5e6ad9daeb5640b6bf9131a
Dirk Weber <Dirk.Weber@rewoo.com>
2019-01-23 17:33:32 +0100

    Works on OIH-Transformer
    Started to transform a Document from/to the OIH format

--- src/main/java/com/rewoo/elastic/actions/TransformDocumentFromOih.java
+++ src/main/java/com/rewoo/elastic/actions/TransformDocumentFromOih.java
@@ -1,15 +1,56 @@
 package com.rewoo.elastic.actions;
 
+import io.elastic.api.EventEmitter;
 import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Message;
 import io.elastic.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 public class TransformDocumentFromOih implements Module {
     private static final Logger logger = LoggerFactory.getLogger(TransformDocumentFromOih.class);
 
     @Override
     public void execute(ExecutionParameters parameters) {
+        JsonObject file = parameters.getMessage().getBody();
+        logger.info("Try to transform file to REWOO scope format");
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
 
+        JsonObject fileVersion = file.getJsonObject("currentVersion");
+        builder.add("elementName", file.getString("path"));
+        builder.add("url", fileVersion.getString("url"));
+        builder.add("type", fileVersion.getString("type"));
+        builder.add("size", fileVersion.getJsonNumber("size").longValue());
+        builder.add("name", fileVersion.getJsonNumber("name").longValue());
+        builder.add("mimeType", fileVersion.getString("mimeType"));
+        builder.add("id", extractAttachmentId(fileVersion));
+        builder.add("hash", extractHash(fileVersion));
+        builder.add("extension", fileVersion.getString("extension"));
+        builder.add("entryId", file.getJsonObject("metadata").getJsonNumber("entryId").longValue());
+        builder.add("elementId", file.getJsonObject("metadata").getJsonNumber("elementId").longValue());
+        builder.add("creationTimestamp", fileVersion.getString("extension"));
+        builder.add("authorId", fileVersion.getString("extension"));
+        logger.info("Transformed file into REWOO scope format");
+        logger.info("Trying to emit file in REWOO scope format to the platform");
+        EventEmitter eventEmitter = parameters.getEventEmitter();
+        final Message data = new Message.Builder().body(builder.build()).build();
+        eventEmitter.emitData(data);
+        logger.info("File emitted to the platform");
+    }
+
+    private static long extractAttachmentId(JsonObject oihDocumentVersion) {
+        String uidString = oihDocumentVersion.getString("uid");
+        return Long.valueOf(uidString.split("-")[1]);
+    }
+
+    private static String extractHash(JsonObject oihDocumentVersion) {
+        String[] urlParts = oihDocumentVersion.getString("url").split("/");
+        return urlParts[urlParts.length - 1];
     }
 }
--- src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
+++ src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
@@ -1,15 +1,65 @@
 package com.rewoo.elastic.actions;
 
+import io.elastic.api.EventEmitter;
 import io.elastic.api.ExecutionParameters;
+import io.elastic.api.Message;
 import io.elastic.api.Module;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.json.*;
 
 public class TransformDocumentToOih implements Module {
     private static final Logger logger = LoggerFactory.getLogger(TransformDocumentToOih.class);
 
     @Override
     public void execute(ExecutionParameters parameters) {
+        JsonObject file = parameters.getMessage().getBody();
+        logger.info("Try to transform file to OIH format");
+        JsonObjectBuilder builder = Json.createObjectBuilder();
+        long attachmentId = file.getJsonNumber("id").longValue();
+        logger.info("File has been identified as attachment with id " + attachmentId);
+        String attachmentUid = "attachment-" + attachmentId;
+        addOihProperties(builder, attachmentUid);
+        builder.add("name", file.getString("name"));
+        builder.add("description", "");
+        builder.add("baseType", file.getString("type"));
+        builder.add("parentUid", file.getString("element-" + file.getJsonNumber("elementId").longValue()));
+        builder.add("path", file.getString("elementName"));
+        builder.add("metadata", Json.createObjectBuilder().add("entryId", file.getJsonNumber("entryId").longValue())
+                                                                .add("elementId", file.getJsonNumber("elementId").longValue()));
+        addFileVersion(builder, file, attachmentUid);
+        logger.info("Transformed file into OIH format");
+        logger.info("Trying to emit file in OIH format to the platform");
+        EventEmitter eventEmitter = parameters.getEventEmitter();
+        final Message data = new Message.Builder().body(builder.build()).build();
+        eventEmitter.emitData(data);
+        logger.info("File emitted to the platform");
+    }
 
+    private static void addFileVersion(JsonObjectBuilder fileBuilder, JsonObject file, String attachmentUid) {
+        JsonObjectBuilder versionBuilder = Json.createObjectBuilder();
+        versionBuilder.add("label", "");
+        versionBuilder.add("comment", "");
+        versionBuilder.add("creation", "");
+        versionBuilder.add("isLatestVersion", true);
+        versionBuilder.add("isMajorVersion", true);
+        versionBuilder.add("size", file.getJsonNumber("size"));
+        versionBuilder.add("mimeType", file.getJsonNumber("mimeType"));
+        versionBuilder.add("url", file.getString("url"));
+        versionBuilder.add("uid", attachmentUid);
+        versionBuilder.add("type", file.getString("type"));
+        versionBuilder.add("extension", file.getString("extension"));
+        fileBuilder.add("currentVersion", versionBuilder.build());
+    }
+
+    private static void addOihProperties(JsonObjectBuilder fileBuilder, String attachmentUid) {
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        fileBuilder.add("oihUid", "");
+        fileBuilder.add("oihCreated",  0);
+        fileBuilder.add("oihLastModified", 0);
+        fileBuilder.add("oihApplicationRecords", arrayBuilder.add(
+                                 Json.createObjectBuilder().add("applicationUid", "")
+                                                           .add("recordUid", attachmentUid)));
     }
 }
