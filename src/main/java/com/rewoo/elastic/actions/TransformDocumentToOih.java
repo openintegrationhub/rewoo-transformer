25fd9d08eece090e1b2ea198b6d4428c688bb4cd
Dirk Weber <Dirk.Weber@rewoo.com>
2019-01-24 14:21:41 +0100

    Works on OIH-Transformer
    Fixed some bug due incorrect json property name

--- src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
+++ src/main/java/com/rewoo/elastic/actions/TransformDocumentToOih.java
@@ -29,7 +29,7 @@ public class TransformDocumentToOih implements Module {
         builder.add("name", file.getString("name"));
         builder.add("description", "");
         builder.add("baseType", file.getString("type"));
-        builder.add("parentUid", file.getString("element-" + file.getJsonNumber("elementId").longValue()));
+        builder.add("parentUid", "element-" + file.getJsonNumber("elementId").longValue());
         builder.add("path", file.getString("elementName"));
         builder.add("metadata", Json.createObjectBuilder().add("entryId", file.getJsonNumber("entryId").longValue())
                                                                 .add("elementId", file.getJsonNumber("elementId").longValue()));
